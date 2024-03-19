97462abbbc71760204c2c43ba10e4e05cf3b12cc
tpoupiot <timfx35@gmail.com>
2024-03-19 16:04:28 +0100

    Méthode -> fonction pour all_import

--- html/data/class_pokemon.js
+++ html/data/class_pokemon.js
@@ -90,79 +90,80 @@ class Class_pokemon {
     }
      */
 
-    static #findGeneration(id) {
-        for (const distinctGeneration in generation) {
-            if (generation[distinctGeneration].find(pokemon => pokemon.id === id)) {
-                return distinctGeneration;
-            }
+}
+
+function findGeneration(id) {
+    for (const distinctGeneration in generation) {
+        if (generation[distinctGeneration].find(pokemon => pokemon.id === id)) {
+            return distinctGeneration;
         }
-        return 0;
     }
-    static #findTypes(id) {
-        let types = {}
+    return 0;
+}
+function findTypes(id) {
+    let types = {}
 
-        pokemon_type
-            .filter(pokemon => pokemon.pokemon_id === id && pokemon.form === 'Normal')
-            .flatMap(pokemon => pokemon.type)
-            .forEach(type => {
-                types[type] = new Class_type(type);
-            });
+    pokemon_type
+        .filter(pokemon => pokemon.pokemon_id === id && pokemon.form === 'Normal')
+        .flatMap(pokemon => pokemon.type)
+        .forEach(type => {
+            types[type] = new Class_type(type);
+        });
 
-        return types;
-    }
-    static #findAttacks(id) {
-        let attacks = {}
+    return types;
+}
+function findAttacks(id) {
+    let attacks = {}
 
-        pokemon_moves
-            .filter(pokemon => pokemon.pokemon_id === id && pokemon.form === 'Normal')
-            .forEach(move => {
-                move.charged_moves.forEach(chargedMove => {
-                    const currentMove = charged_moves.find(move => move.name === chargedMove);
+    pokemon_moves
+        .filter(pokemon => pokemon.pokemon_id === id && pokemon.form === 'Normal')
+        .forEach(move => {
+            move.charged_moves.forEach(chargedMove => {
+                const currentMove = charged_moves.find(move => move.name === chargedMove);
 
-                    if (currentMove) {
-                        attacks[chargedMove] = new Class_attack(
-                            currentMove.move_id,
-                            currentMove.name,
-                            currentMove.type,
-                            currentMove.power,
-                            currentMove.duration,
-                            currentMove.energy_delta,
-                            currentMove.stamina_loss_scaler,
-                            currentMove.critical_chance ?? 0,
-                            "Charged move"
-                        )
-                    }
-                })
-
-                move.fast_moves.forEach(fastMove => {
-                    const currentMove = fast_moves.find(move => move.name === fastMove);
-
-                    if (currentMove) {
-                        attacks[fastMove] = new Class_attack(
-                            currentMove.move_id,
-                            currentMove.name,
-                            currentMove.type,
-                            currentMove.power,
-                            currentMove.duration,
-                            currentMove.energy_delta,
-                            currentMove.stamina_loss_scaler,
-                            0,
-                            "Fast move"
-                        )
-                    }
-                })
+                if (currentMove) {
+                    attacks[chargedMove] = new Class_attack(
+                        currentMove.move_id,
+                        currentMove.name,
+                        currentMove.type,
+                        currentMove.power,
+                        currentMove.duration,
+                        currentMove.energy_delta,
+                        currentMove.stamina_loss_scaler,
+                        currentMove.critical_chance ?? 0,
+                        "Charged move"
+                    )
+                }
             })
 
-        return attacks
-    }
+            move.fast_moves.forEach(fastMove => {
+                const currentMove = fast_moves.find(move => move.name === fastMove);
 
+                if (currentMove) {
+                    attacks[fastMove] = new Class_attack(
+                        currentMove.move_id,
+                        currentMove.name,
+                        currentMove.type,
+                        currentMove.power,
+                        currentMove.duration,
+                        currentMove.energy_delta,
+                        currentMove.stamina_loss_scaler,
+                        0,
+                        "Fast move"
+                    )
+                }
+            })
+        })
 
-    static import_pokemon() {
-        pokemon
+    return attacks
+}
+
+function import_pokemon() {
+    pokemon
         .filter(currentPokemon => currentPokemon.form === 'Normal')
         .forEach(currentPokemon => {
-            const currentTypes = Class_pokemon.#findTypes(currentPokemon.pokemon_id)
-            const currentAttacks = Class_pokemon.#findAttacks(currentPokemon.pokemon_id)
+            const currentTypes = findTypes(currentPokemon.pokemon_id)
+            const currentAttacks = findAttacks(currentPokemon.pokemon_id)
 
             Object.entries(currentTypes).forEach(type => {
                 if (!Class_type.all_types[type[1]]) {
@@ -176,23 +177,25 @@ class Class_pokemon {
                 }
             });
 
-            Class_pokemon.all_pokemon[currentPokemon.pokemon_id] =
-                new Class_pokemon(
-                    currentPokemon.pokemon_id,
-                    currentPokemon.pokemon_name,
-                    currentPokemon.base_attack,
-                    currentPokemon.base_defense,
-                    currentPokemon.base_stamina,
-                    Class_pokemon.#findGeneration(currentPokemon.pokemon_id),
-                    1,
-                    cp_multiplier.find(multiplier => multiplier.level === 1).multiplier,
-                    currentTypes,
-                    currentAttacks
-                );
-        });
-    }
+            const currentGeneration = findGeneration(currentPokemon.pokemon_id);
 
+            if (generation[currentGeneration]) {
+                Class_pokemon.all_pokemon[currentPokemon.pokemon_id] =
+                    new Class_pokemon(
+                        currentPokemon.pokemon_id,
+                        currentPokemon.pokemon_name,
+                        currentPokemon.base_attack,
+                        currentPokemon.base_defense,
+                        currentPokemon.base_stamina,
+                        currentGeneration,
+                        1,
+                        cp_multiplier.find(multiplier => multiplier.level === 1).multiplier,
+                        currentTypes,
+                        currentAttacks
+                    );
+            }
+        });
 }
 
 
-Class_pokemon.import_pokemon();
+import_pokemon();
